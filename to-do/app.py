ea8beae2a3fa6e8fc21d0994b5179a6e4301ba44
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2024-09-07 11:58:23 +0530

    added todo work

--- to-do/app.py
+++ to-do/app.py
@@ -1,8 +1,23 @@
+from zoneinfo import ZoneInfo
 from flask import Flask, render_template # type: ignore
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+indian_time = datetime.now(ZoneInfo("Asia/Kolkata"))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///todo.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default = indian_time)
+
+    def __repr__(self):
+        return '<Task %r>' %self.id
 
 @app.route('/')
 
